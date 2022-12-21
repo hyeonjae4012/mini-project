6d3c0fad70ff069f8a9ab85f44cca7a418a431be
hyeonjae4012 <jung.hyeonjae@classmethod.jp>
2022-12-21 14:29:44 +0900

    add app-stack and create s3 bucket for status web site hosting

---
+++ lib/app-stack/app-stack.ts
@@ -0,0 +1,28 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as iam from 'aws-cdk-lib/aws-iam'
+
+export class AppStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    const appHostingBucket = new s3.Bucket(this, 'mini-project-front-source', {
+        blockPublicAccess: s3.BlockPublicAccess.BLOCK_ACLS,
+        removalPolicy: cdk.RemovalPolicy.DESTROY,
+        websiteIndexDocument: 'index.html',
+      });
+  
+  
+      const appHostingBucketPolicyStatement = new iam.PolicyStatement({
+        actions: ['s3:GetObject'],
+        effect: iam.Effect.ALLOW,
+        principals: [
+          new iam.AnyPrincipal
+        ],
+        resources: [`${appHostingBucket.bucketArn}/*`]
+      });
+  
+      appHostingBucket.addToResourcePolicy(appHostingBucketPolicyStatement);
+  }
+}
