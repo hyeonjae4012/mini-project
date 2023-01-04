8eb0dc42131d4407d1ef7c71321e9895811483a2
hyeonjae4012 <jung.hyeonjae@classmethod.jp>
2023-01-04 16:34:44 +0900

    change lambda return body

--- src/handler/app.ts
+++ src/handler/app.ts
@@ -2,9 +2,7 @@ export const handler = async (event: any) => {
   console.log(JSON.stringify(event))
       
   return {
-      isBase64Encoded: true,
-      headers: {"testHeaderKey": "testHeaderValue"},
       statuscode: 200,
-      body: 'Hello CDK',
+      body: JSON.stringify('Hello from Lambda!'),
   }
 }
