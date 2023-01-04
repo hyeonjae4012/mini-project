a9cd49c7cd7de61545d2a3dd3d0623df22957804
hyeonjae4012 <jung.hyeonjae@classmethod.jp>
2023-01-04 16:38:18 +0900

    fix typing error

--- src/handler/app.ts
+++ src/handler/app.ts
@@ -2,7 +2,7 @@ export const handler = async (event: any) => {
   console.log(JSON.stringify(event))
       
   return {
-      statuscode: 200,
+      statusCode: 200,
       body: JSON.stringify('Hello from Lambda!'),
   }
 }
